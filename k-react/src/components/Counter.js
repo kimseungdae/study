d2337091b5d30e2a84becaee266eeb4357f3522d
kimseungdae <kocacolla@naver.com>
2020-09-16 13:13:48 +0900

    20200916

---
+++ k-react/src/components/Counter.js
@@ -0,0 +1,12 @@
+import React, {Component} from "react";
+class Counter extends Component{
+
+    render() {
+        return (
+            '테스트 테스트'
+
+        );
+    }
+}
+
+export default Counter;
--- k-react/src/components/Movie.js
+++ k-react/src/components/Movie.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import '../css/Movie.css';
+import Counter from './Counter'
 
 class Movie extends Component {
     render() {
@@ -8,6 +9,7 @@ class Movie extends Component {
             <div>
                 <MoviePoster />
                 <h1>{this.props.title}</h1>
+                <Counter />
             </div>
         );
     }
